93d605bab3490ae504874829e05c719959edc199
Wilhelm Kirchgässner <16706935+wkirgsn@users.noreply.github.com>
2021-09-03 09:32:31 +0200

    update prototype with trainin routine. still broken.

---
+++ examples/thermal_modeling/bin/pmsm_temp_est.jl
@@ -0,0 +1,105 @@
+using Flux, DataFrames, CSV, ProgressMeter
+using Statistics: mean
+using thermal_modeling: TNNCell
+
+data = CSV.File(joinpath(pwd(), "..", "data", "measures.csv")) |> DataFrame
+
+# FE
+@. data[!, :i_norm] = sqrt(data.i_d^2 + data.i_q^2)
+data[!, :fe1] = data.i_norm / maximum(data.i_norm) .* data.motor_speed / maximum(data.motor_speed);
+
+# normalization
+# todo
+
+test_set_pids = [60, 62, 74]
+target_cols = ["pm", "stator_tooth", "stator_winding", "stator_yoke"]
+gdf = groupby(data, :profile_id)
+
+p_sizes = combine(gdf, nrow)
+max_len_test = maximum(filter(:profile_id => n -> n in test_set_pids, p_sizes).nrow)
+max_len_train = maximum(filter(:profile_id => n -> n ∉ test_set_pids, p_sizes).nrow)
+
+n_test_profiles = length(test_set_pids)
+n_train_profiles = length(keys(gdf)) - n_test_profiles
+
+c_input_temps = ["ambient", "coolant"]
+c_temps = [target_cols..., c_input_temps...]
+c_non_temps = [c for c in names(data) if c ∉ [c_temps..., "profile_id"]]
+
+train_tensor_non_temp_x = zeros(Float32, (max_len_train, length(c_non_temps), n_train_profiles))
+train_tensor_temps_x = zeros(Float32, (max_len_train, length(c_input_temps), n_train_profiles))
+train_tensor_y = zeros(Float32, (max_len_train, length(target_cols), n_train_profiles))
+train_sample_weights = zeros(Float32, (max_len_train, n_train_profiles))
+
+test_tensor_non_temp_x = zeros(Float32, (max_len_test, length(c_non_temps), n_test_profiles))
+test_tensor_temps_x = zeros(Float32, (max_len_test, length(c_input_temps), n_test_profiles))
+test_tensor_y = zeros(Float32, (max_len_test, length(target_cols), n_test_profiles))
+test_sample_weights = zeros(Float32, (max_len_test, n_test_profiles));
+
+# fill in DataFrame information
+test_p_idx = 0
+train_p_idx = 0
+@showprogress 0.5 "Generate tensors " for (pid, df) in pairs(gdf)
+    if pid.profile_id ∈ test_set_pids
+        global test_p_idx += 1
+        test_tensor_non_temp_x[1:nrow(df), :, test_p_idx] .= df[:, c_non_temps]
+        test_tensor_temps_x[1:nrow(df), :, test_p_idx] .= df[:, c_input_temps]
+        test_tensor_y[1:nrow(df), :, test_p_idx] .= df[:, target_cols]
+        test_sample_weights[1:nrow(df), test_p_idx] .= 1
+    else
+        global train_p_idx += 1
+        train_tensor_non_temp_x[1:nrow(df), :, train_p_idx] .= df[:, c_non_temps]
+        train_tensor_temps_x[1:nrow(df), :, train_p_idx] .= df[:, c_input_temps]
+        train_tensor_y[1:nrow(df), :, train_p_idx] .= df[:, target_cols]
+        train_sample_weights[1:nrow(df), train_p_idx] .= 1
+    end
+end
+
+tbptt_len = 128
+
+train_vec_temps_x = [train_tensor_temps_x[i, :, :] for i in 1:size(train_tensor_temps_x, 1)]
+train_vec_non_temp_x = [train_tensor_non_temp_x[i, :, :] for i in 1:size(train_tensor_non_temp_x, 1)]
+train_vec_x = collect(zip(train_vec_non_temp_x, train_vec_temps_x))
+train_vec_y = [train_tensor_y[i, :, :] for i in 1:size(train_tensor_y, 1)]
+train_vec_sample_weights = [train_sample_weights[i, :] for i in 1:size(train_sample_weights, 1)]
+
+train_vec_chunked_x = []
+train_vec_chunked_y = []
+train_vec_chunked_w = []
+
+i = 0;
+while i*tbptt_len <= max_len_train
+    push!(train_vec_chunked_x, train_vec_x[i*tbptt_len+1:minimum(((i+1)*tbptt_len+1, max_len_train))])
+    push!(train_vec_chunked_y, train_vec_y[i*tbptt_len+1:minimum(((i+1)*tbptt_len+1, max_len_train))])
+    push!(train_vec_chunked_w, train_vec_sample_weights[i*tbptt_len+1:minimum(((i+1)*tbptt_len+1, max_len_train))])
+    global i+= 1
+end
+
+test_vec_temps_x = [test_tensor_temps_x[i, :, :] for i in 1:size(test_tensor_temps_x, 1)]
+test_vec_non_temp_x = [test_tensor_non_temp_x[i, :, :] for i in 1:size(test_tensor_non_temp_x, 1)]
+test_vec_x = collect(zip(train_vec_non_temp_x, test_vec_temps_x))
+test_vec_y = [test_tensor_y[i, :, :] for i in 1:size(test_tensor_y, 1)]
+test_vec_sample_weights = [test_sample_weights[i, :] for i in 1:size(test_sample_weights, 1)];
+
+n_epochs = 100
+pbar = Progress(n_epochs, desc="Training Epochs", start=1, showspeed=true)
+init_hidden = train_vec_y[1]
+m = Flux.Recur(TNNCell(length(c_non_temps)+length(c_input_temps),
+                       length(c_temps),
+                       length(target_cols),
+                       init_hidden),
+               init_hidden)
+ps = params(m)
+opt = ADAM(1e-3)
+
+function sample_weighted_loss(x::Vector{Tuple{Matrix{T}, Matrix{T}}}, y::Vector{U}, w::Vector{V}) where {T, U, V}
+   mean(Flux.Losses.mse(m(xi), yi, agg=z->mean(wi' .* z)) for (xi, yi, wi) in zip(x, y, w)) 
+end
+
+# training
+data_tup = zip(train_vec_chunked_x, train_vec_chunked_y, train_vec_chunked_w);
+for epoch in 1:n_epochs
+    Flux.reset!(m)
+    Flux.train!(sample_weighted_loss, ps, data_tup, opt)
+    next!(pbar, showvalues = [(:epoch, epoch)])
+end
--- examples/thermal_modeling/src/thermal_modeling.jl
+++ examples/thermal_modeling/src/thermal_modeling.jl
@@ -2,13 +2,6 @@ module thermal_modeling
 
 using Flux
 
-const n_input_temps = 2
-const n_non_input_temps = 3
-const n_total_inputs = n_input_temps + n_non_input_temps
-const n_targets = 3
-const n_temps = n_targets + n_input_temps
-const n_profiles = 49
-
 
 mutable struct HeatTransferLayer{U, V, T}
     n_temps::Int
@@ -17,7 +10,7 @@ mutable struct HeatTransferLayer{U, V, T}
     adj_mat::Matrix{Int8}
 end
 
-function HeatTransferLayer(n_temps::Integer, n_targets::Integer)
+function HeatTransferLayer(n_input::Integer, n_temps::Integer, n_targets::Integer)
     # populate adjacency matrix
     adj_mat = zeros(Int8, n_temps, n_temps)
     k = 1
@@ -30,15 +23,15 @@ function HeatTransferLayer(n_temps::Integer, n_targets::Integer)
     adj_mat = adj_mat + adj_mat'
     n_conds = Int(0.5 * n_temps * (n_temps - 1))
     HeatTransferLayer(n_temps, n_targets,
-                      Dense(n_total_inputs + n_targets, n_conds, σ),
+                      Dense(n_input + n_targets, n_conds, σ),
                       adj_mat)
 end
 
 # overload struct to make it callable
 function (m::HeatTransferLayer)(all_input)
     n_temps = m.n_temps
-    prev_out = @views all_input[1:m.n_targets, :]
-    temps = @views all_input[1:n_temps, :]
+    prev_out = @view all_input[1:m.n_targets, :]
+    temps = @view all_input[1:n_temps, :]
     
     conductances = m.conductance_net(all_input)
     
@@ -47,6 +40,7 @@ function (m::HeatTransferLayer)(all_input)
     for i in 1:m.n_targets
         for j in 1:n_temps
             if j != i
+                # todo: Mutating arrays apparently not supported. Think of something different!
                 @. tmp[i, :] += (temps[j, :] - prev_out[i, :]) * conductances[m.adj_mat[i, j], :]
             end
         end
@@ -55,26 +49,28 @@ function (m::HeatTransferLayer)(all_input)
     return tmp
 end
 
-Flux.@functor HeatTransferLayer
+# specify what is trainable 
+Flux.@functor HeatTransferLayer (conductance_net, )
 
-mutable struct TNNCell{U <:Chain, V<:Real}
+mutable struct TNNCell{U <:Chain, V<:Real, S}
     sample_time::V
     ploss_net::U
     heat_net::HeatTransferLayer
     caps:: Vector{V}
     prll::Parallel  # will be defined in inner constructor (no outer definition)
-    function TNNCell(sample_time::V, ploss_net::U, heat_net::HeatTransferLayer, caps::Vector{V}) where {U <: Chain, V <: Real}
-        new{U, V}(sample_time, ploss_net, heat_net, caps, Parallel(+, ploss_net, heat_net))
+    state0:: S
+    function TNNCell(sample_time::V, ploss_net::U, heat_net::HeatTransferLayer, caps::Vector{V}, init_hidden::S) where {U <: Chain, V <: Real, S}
+        new{U, V, S}(sample_time, ploss_net, heat_net, caps, Parallel(+, ploss_net, heat_net), init_hidden)
     end
 end
 
 
-function TNNCell()
-    ploss_net = Chain(Dense(n_total_inputs + n_targets, 8, σ),
+function TNNCell(n_input::U, n_temps::U, n_targets::U, init_hidden::S) where {U <: Integer, S}
+    ploss_net = Chain(Dense(n_input + n_targets, 8, σ),
                       Dense(8, n_targets, σ))
-    heat_transfer = HeatTransferLayer(n_temps, n_targets)
+    heat_transfer = HeatTransferLayer(n_input, n_temps, n_targets)
     caps = 0.5f0 .* randn(Float32, n_targets) .- 3f0  # Gaussian mean=-3 std=0.5
-    TNNCell(Float32(0.5), ploss_net, heat_transfer, caps)
+    TNNCell(Float32(0.5), ploss_net, heat_transfer, caps, init_hidden)
 end
 
 function (m::TNNCell)(prev_̂y, x)
@@ -85,6 +81,8 @@ function (m::TNNCell)(prev_̂y, x)
     return y, prev_̂y
 end
 
+# specify what is trainable 
+Flux.@functor TNNCell (ploss_net, heat_net, caps)
 
 end # module
 	
